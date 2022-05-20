4bfd57583fce3872d31ac7b80310c2e3eecac085
lemphis <lemphis@gmail.com>
2022-05-20 17:57:05 +0900

    Initial commit

---
+++ src/test/kotlin/com/demo/kafkaelk/KafkaElkApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.demo.kafkaelk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KafkaElkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
