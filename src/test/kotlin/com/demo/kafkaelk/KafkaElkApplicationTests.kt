d072653bf90cb9f272473e237d774081caadc46d
lemphis <lemphis@gmail.com>
2022-05-30 17:28:36 +0900

    Wip
    * ELK

--- src/test/kotlin/com/demo/kafkaelk/KafkaElkApplicationTests.kt
+++ src/test/kotlin/com/demo/kafkaelk/KafkaElkApplicationTests.kt
@@ -3,7 +3,7 @@ package com.demo.kafkaelk
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class KafkaElkApplicationTests {
 
 	@Test
